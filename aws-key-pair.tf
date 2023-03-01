8fe0a97cff69a38f51c9c71d0f622434d523fe77
shashank-kumar-rajput <shashank4717@gmail.com>
2023-03-01 22:37:24 +0530

    key pair update

--- aws-key-pair.tf
+++ aws-key-pair.tf
@@ -2,7 +2,7 @@
 resource "aws_key_pair" "key-tf"{
     key_name= "${var.key_name}"
 #    public_key= "${var.public_key}"
-   public_key= file("${path.module}/id_rsa3.pub")
+   public_key= file("${var.public_key}")
 }
 
 output "key_name"{
--- instance.tf
+++ instance.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "ec2_terraform1"{
  instance_type= "${var.ec2_instance_type1}"
  key_name="${aws_key_pair.key-tf.key_name}"
  vpc_security_group_ids = ["${aws_security_group.Custome-security-group-tf.id}"]
- user_data = file("${path.module}/script.sh")
+ user_data = file("${script.sh}")
  tags={
     Name="${var.ec2_instance_name}"
  }
