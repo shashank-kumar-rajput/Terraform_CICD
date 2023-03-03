c407a967a20639b8543eb3d421418467244363cc
shashank-kumar-rajput <shashank4717@gmail.com>
2023-03-03 16:03:20 +0530

    terraform backend s3 --reconfig

--- backen.tf
+++ backen.tf
@@ -1,10 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "tf-aws-state-bucket"
+    bucket = "tf-aws-state-bucket1"
     key = "main"
     region = "us-east-1"
     profile = "default"
-
     dynamodb_table = "tf-dynamo-db-table"
   }
 }
